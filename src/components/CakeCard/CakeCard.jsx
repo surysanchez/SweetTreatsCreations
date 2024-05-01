20b36b1df77229ae7fac58755bf48083efe30746
suryc <surysanchezsales@gmail.com>
2024-05-01 09:56:50 -0400

    update style

--- src/components/CakeCard/CakeCard.jsx
+++ src/components/CakeCard/CakeCard.jsx
@@ -1,33 +1,34 @@
-import Button from "react-bootstrap/Button";
-import Card from "react-bootstrap/Card";
-import Container from "react-bootstrap/Container";
+// import Button from "react-bootstrap/Button";
+// import Card from "react-bootstrap/Card";
+// import Container from "react-bootstrap/Container";
 
-export default function CakeCard({cake}) {
-  return (
-  <>
-  <Container fluid >
+// export default function CakeCard({cake}) {
+//   return (
+//   <>
+//   <Container fluid >
         
-        {/* <Link to={`/customtreat/${props.custom.name}`} className="treat-link"> */}
-        <Card className="treat-card">
-          <Card.Img
-            style={{
-              background: "no-repeat center",
-              WebkitBackgroundSize: "cover",
-            }}
-            variant="top"
-            src={cake.picture}
-            className="img"
-          />
-          <Card.Body>
-            <Card.Title>{cake.name}</Card.Title>
-            {/* <Card.Text></Card.Text> */}
-            {/* <Button variant="primary" */}
+//         {/* <Link to={`/customtreat/${props.custom.name}`} className="treat-link"> */}
+//         <Card className="treat-card">
+//           <Card.Img
+//             style={{
+//               background: "no-repeat center",
+//               WebkitBackgroundSize: "cover",
+//             }}
+//             variant="top"
+//             src={cake.picture}
+//             className="img"
+//           />
+//           <Card.Body>
+//             <Card.Title>{cake.name}</Card.Title>
+//             {/* <Card.Text></Card.Text> */}
+//             {/* <Button variant="primary" */}
 
-            {/* </Button> */}
-          </Card.Body>
-        </Card>
-      </Container>
-  </>
-      
-  );
-}
+//             {/* </Button> */}
+//           </Card.Body>
+//         </Card>
+//       </Container>
+//   </>
+ 
+//   );
+// }
+
